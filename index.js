671855e292663540c42e5c90bc6552744a349d91
Benjamin Herbert <benjaminherbert@MAC-422.lan>
2023-05-10 21:42:35 -0400

    trying to force https

--- index.js
+++ index.js
@@ -25,6 +25,15 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use(methodOverride('_method'))
 
+app.use(requireHTTPS)
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+      return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+  }
+
 // '/' is the url path,
 app.get('/', async (request, response) => { { 
 
